10190a76155f5bd7127787e880cdfb3118fb65fd
Andrés Laurizi <andres.laurizi@taligent.com.ar>
2026-08-25 16:52:28 -0300

    Primer commit

---
+++ metricas/ventas/cantidad_clientes.sql
@@ -0,0 +1,11 @@
+sql
+-- metrica: Cantidad de clientes activos
+-- dueño: tu nombre
+-- descripcion: Clientes distintos con al menos una compra en el período.
+
+SELECT
+    periodo,
+    COUNT(DISTINCT cliente) AS clientes_activos
+FROM ventas.comprobantes
+WHERE anulado = 0
+GROUP BY periodo;
